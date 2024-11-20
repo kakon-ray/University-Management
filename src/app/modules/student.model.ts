d2ac61bce4ac1416206c4e4dd571244648cd0e4a
Kakon Ray <thisiskakonray@gmail.com>
2024-11-20 09:41:08 +0600

    Custome Validation Done

--- src/app/modules/student.model.ts
+++ src/app/modules/student.model.ts
@@ -9,10 +9,20 @@ import {
 const userNameSchema = new Schema<UserName>({
   firstName: {
     type: String,
-    required: true,
+    required: [true, 'Firstname is required'],
+    maxlength: [20, 'First Name can not be more then 20'],
+    validate: {
+      validator: function (value: string) {
+        const firstName =
+          value.charAt(0).toUpperCase() + value.slice(1).toLowerCase()
+        return firstName === value
+      },
+      message: '{VALUE} is not in Capitalize formate',
+    },
   },
   middleName: {
     type: String,
+    trim: true,
   },
   lastName: {
     type: String,
@@ -67,38 +77,63 @@ const localGuardian = new Schema<LocalGuardian>({
 })
 
 const studentSchema = new Schema<Student>({
-  id: { type: String },
-  name: userNameSchema,
-  gender: ['male', 'female'],
+  id: { type: String, required: true, unique: true },
+  name: {
+    type: userNameSchema,
+    required: true,
+  },
+  gender: {
+    type: String,
+    enum: {
+      values: ['male', 'female', 'Other'],
+      message: '{VALUE} is not valid',
+    },
+    required: true,
+  },
   email: {
     type: String,
-    required: true,
+    required: [true, 'Email is required'],
+    unique: true,
   },
   dateOfBirth: String,
   contactNo: {
     type: String,
-    required: true,
+    required: [true, 'Contact No is required'],
   },
   emerganceyContactNo: {
     type: String,
     required: true,
   },
-  bloodGroop: ['A+', 'B+', 'O+', 'A-', 'B-'],
+  bloodGroop: {
+    type: String,
+    enum: ['A+', 'B+', 'O+', 'A-', 'B-'],
+    required: [true, 'Blood Groop is required'],
+  },
   presentAddress: {
     type: String,
-    required: true,
+    required: [true, 'Present Address is required'],
   },
   permanentAddress: {
     type: String,
+    required: [true, 'Permanent Address is required'],
+  },
+
+  guardian: {
+    type: guardianSchema,
+    required: [true, 'Guardian is required'],
+  },
+
+  localGuardian: {
+    type: localGuardian,
     required: true,
   },
 
-  guardian: guardianSchema,
-
-  localGuardian: localGuardian,
-
   profileImage: { type: String },
-  isActice: ['active', 'blocked'],
+  isActice: {
+    type: String,
+    enum: ['active', 'blocked'],
+    default: 'active',
+  },
 })
 
 // 3. Create a Model.

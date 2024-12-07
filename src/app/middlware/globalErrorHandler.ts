3beb9d22d90ec1d0e6efe05f544e265b1540e3d1
Fouzder Tech <fouzdertech@gmail.com>
2024-12-07 17:27:48 +0600

    Zod Validation Error Done

--- src/app/middlware/globalErrorHandler.ts
+++ src/app/middlware/globalErrorHandler.ts
@@ -1,23 +1,58 @@
-import { NextFunction, Request, Response } from 'express'
+import { ErrorRequestHandler, NextFunction, Request, Response } from 'express'
+import { ZodError, ZodIssue } from 'zod'
+import { TErrorSource } from '../interface/error'
+import config from '../config'
 
-const globalErrorHandler = (
-  err: any,
-  req: Request,
-  res: Response,
-  next: NextFunction,
+
+const globalErrorHandler : ErrorRequestHandler = (
+  err,
+  req,
+  res,
+  next,
 ) => {
-  const statusCode = err.statusCode || 500
-  const message = err?.message || 'An unexpected error occurred!'
+  let statusCode = err.statusCode || 500
+  let message = err?.message || 'An unexpected error occurred!'
 
-  if (err.stack) {
-    console.error(err.stack)
+
+
+ 
+  let errorSources:TErrorSource = [{
+     path: '',
+     message: 'Something went wrong'
+  }]
+
+
+  const handleZodError = (err: ZodError) => {
+
+    const errorSource:TErrorSource = err.issues.map((issue:ZodIssue) => {
+     return{
+      path:issue?.path[issue.path.length-1],
+      message:issue?.message
+     }
+    })
+
+    const statusCode = 400;
+    return {
+        statusCode,
+        message: 'Zod Validation Error',
+        errorSource
+    }
   }
+
+  if(err instanceof ZodError){
+    const simplifiedError = handleZodError(err)
+
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorSources = simplifiedError.errorSource;
+  }
+
   // console.error(err)
-  res.status(statusCode).json({
+  return res.status(statusCode).json({
     success: false,
     message,
-    errorDetails:
-      process.env.NODE_ENV === 'development' ? err.stack : undefined,
+    errorSources,
+    stack: config.NODE_ENV === 'development' ? err.stack : null,
   })
 }
 

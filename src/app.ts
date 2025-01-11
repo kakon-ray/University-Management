99620fcf883e4608e48377232adaaf8489b62bb4
Kakon Ray <thisiskakonray@gmail.com>
2025-01-11 16:49:12 +0600

    Cors and credentials done

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ const app: Application = express()
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({origin:['http://localhost:3000']}))
+app.use(cors({ origin: 'http://localhost:5174', credentials: true }))
 
 // test route
 const testRoute = (req: Request, res: Response) => {

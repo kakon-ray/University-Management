3d7b15b659edea950cf9ee64ca3384e9490d803a
Kakon Ray <thisiskakonray@gmail.com>
2025-01-13 00:21:56 +0600

    Done

--- src/app/middlware/auth.ts
+++ src/app/middlware/auth.ts
@@ -1,69 +1,76 @@
-import { NextFunction, Request, Response } from "express";
-import { AnyZodObject } from "zod";
-import catchAsync from "../utils/catchAsync";
-import AppError from "../errors/AppError";
-import httpStatus from "http-status";
-import jwt, { JwtPayload } from "jsonwebtoken";
-import config from "../config";
-import { TuserRole } from "../modules/users/user.interface";
-import { User } from "../modules/users/user.model";
-import { Admin } from "../modules/admin/admin.model";
+import { NextFunction, Request, Response } from 'express'
+import { AnyZodObject } from 'zod'
+import catchAsync from '../utils/catchAsync'
+import AppError from '../errors/AppError'
+import httpStatus from 'http-status'
+import jwt, { JwtPayload } from 'jsonwebtoken'
+import config from '../config'
+import { TuserRole } from '../modules/users/user.interface'
+import { User } from '../modules/users/user.model'
+import { Admin } from '../modules/admin/admin.model'
 
 const auth = (...requiredRoles: TuserRole[]) => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-    const token = req.headers.authorization;
+    const token = req.headers.authorization
 
     // checking is if the token not provide
     if (!token) {
-      throw new AppError(httpStatus.UNAUTHORIZED, "You are not authorized !");
+      throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized !')
     }
 
-    const decoded = jwt.verify(
-      token,
-      config.jwt_access_secret as string
-    ) as JwtPayload;
+    let decoded
+    try {
+      decoded = jwt.verify(
+        token,
+        config.jwt_access_secret as string,
+      ) as JwtPayload
+    } catch (err) {
+      throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized')
+    }
 
-    const {role, userId, iat} = decoded
+    const { role, userId, iat } = decoded
 
-    const user = await User.isUserExistsByCustomId(userId);
+    const user = await User.isUserExistsByCustomId(userId)
 
     // checking if the user not found
     if (!user) {
-      throw new AppError(
-        400,
-        "User does not exists                                                                                                                                                                   "
-      );
+      throw new AppError(httpStatus.NOT_FOUND, 'User does not exists')
     }
 
-    
     // checking if the admin is soft deleted
     if (user?.isDeleted) {
-      throw new AppError(400, "The User is Deleted");
+      throw new AppError(400, 'The User is Deleted')
     }
-
 
     // checking if the user is blocked
-    if (user?.status == "blocked") {
-      throw new AppError(400, "The user is blocked");
+    if (user?.status == 'blocked') {
+      throw new AppError(400, 'The user is blocked')
     }
 
-    
     // if user chnage password after login checking
-    if(user.passwordChnageAt && await User.isJwtIssuedBeforePasswordChanged(user.passwordChnageAt, iat as number)){
-      throw new AppError(httpStatus.UNAUTHORIZED, "You are not authorized please login !");
+    if (
+      user.passwordChnageAt &&
+      (await User.isJwtIssuedBeforePasswordChanged(
+        user.passwordChnageAt,
+        iat as number,
+      ))
+    ) {
+      throw new AppError(
+        httpStatus.UNAUTHORIZED,
+        'You are not authorized please login !',
+      )
     }
-
 
     if (requiredRoles && !requiredRoles.includes(role)) {
       throw new AppError(
         httpStatus.UNAUTHORIZED,
-        "You are not outhorized from this role"
-      );
+        'You are not outhorized from this role',
+      )
     }
 
-    req.user = decoded as JwtPayload;
-    next();
-  });
-};
+    req.user = decoded as JwtPayload
+    next()
+  })
+}
 
-export default auth;
+export default auth

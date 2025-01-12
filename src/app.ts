d1ae2da724d0fe3c4a70c1e18dd4e8685fac21ed
Kakon Ray <thisiskakonray@gmail.com>
2025-01-12 23:55:31 +0600

    add academic semester auth guard

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ const app: Application = express()
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({ origin: 'http://localhost:5174', credentials: true }))
+app.use(cors({ origin: 'http://localhost:5173', credentials: true }))
 
 // test route
 const testRoute = (req: Request, res: Response) => {
--- src/app/modules/academicSemester/academicSemester.route.ts
+++ src/app/modules/academicSemester/academicSemester.route.ts
@@ -6,19 +6,31 @@ import {
   createAcademicSemesterValidationSchema,
   updateAcademicSemesterValidationSchema,
 } from './academicSemester.validation'
+import { USER_ROLE } from '../users/user.constant'
+import auth from '../../middlware/auth'
 
 const router = express.Router()
 
 router.post(
   '/create',
+  auth(USER_ROLE.admin),
   validateRequest(createAcademicSemesterValidationSchema),
   AcademicSemeterController.createAcademicSemester,
 )
 
-router.get('/get', AcademicSemeterController.getAcademicSemester)
-router.get('/get/:id', AcademicSemeterController.getSingleAcademicSemester)
+router.get(
+  '/get',
+  auth(USER_ROLE.admin),
+  AcademicSemeterController.getAcademicSemester,
+)
+router.get(
+  '/get/:id',
+  auth(USER_ROLE.admin),
+  AcademicSemeterController.getSingleAcademicSemester,
+)
 router.patch(
   '/update/:id',
+  auth(USER_ROLE.admin),
   validateRequest(updateAcademicSemesterValidationSchema),
   AcademicSemeterController.updateAcademicSemester,
 )
